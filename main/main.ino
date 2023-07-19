8b011d96bb83ff6ed7e4fc1c3beb2e345ad172d0
Donghyeon Lee <earthicko@gmail.com>
2023-07-20 07:59:19 +0900

    Feat: add USB HID msg composer

--- main/main.ino
+++ main/main.ino
@@ -3,8 +3,10 @@
 #include "MsgPipe.hpp"
 #include "PS2CodeManager.hpp"
 #include "PS2Mouse.hpp"
+#include "USBHIDKeys.hpp"
 #include <Arduino.h>
 #include <PS2KeyRaw.h>
+#include <cstring>
 
 const int MOUSE_MASK_LEFT = 0x01;
 const int MOUSE_MASK_RIGHT = 0x02;
@@ -124,6 +126,53 @@ void handleMouse(void)
 	}
 }
 
+void composeKeyboardMsg(hidmsg_t &msg, uint8_t keycode, bool pressed)
+{
+	if (keycode == KEY_MOD_LCTRL || keycode == KEY_MOD_LSHIFT || keycode == KEY_MOD_LALT
+		|| keycode == KEY_MOD_LMETA || keycode == KEY_MOD_RCTRL || keycode == KEY_MOD_RSHIFT
+		|| keycode == KEY_MOD_RALT || keycode == KEY_MOD_RMETA)
+	{
+		if (pressed)
+			msg.buf[0] |= keycode;
+		else
+			msg.buf[0] &= ~keycode;
+	}
+	else
+	{
+		if (pressed)
+		{
+			bool inserted = false;
+			for (uint8_t i = 2; i < 8; i++)
+			{
+				if (msg.buf[i] == 0)
+				{
+					msg.buf[i] = keycode;
+					inserted = true;
+					break;
+				}
+			}
+			if (!inserted)
+			{
+				// TODO: handle too many keys
+				DEBUG_PRINTF("Too many keys pressed\n");
+			}
+		}
+		else
+		{
+			for (uint8_t i = 2; i < 8; i++)
+			{
+				if (msg.buf[i] == keycode)
+				{
+					for (uint8_t j = i; j < 7; j++)
+						msg.buf[j] = msg.buf[j + 1];
+					msg.buf[7] = 0;
+					break;
+				}
+			}
+		}
+	}
+}
+
 void handleKeyboard(void)
 {
 	if (keyboard.available() == 0)
@@ -136,16 +185,33 @@ void handleKeyboard(void)
 	uint8_t updown;
 	if (PS2CodeManager::parse(c, &keycode, &updown))
 	{
+		static hidmsg_t msg;
+		bool pressed;
+
+		msg.ep = 1;
+		msg.len = 8;
+
 		DEBUG_PRINTF("Parsed USB keycode %X, ", keycode);
 		switch (updown)
 		{
 		case PS2CodeManager::BREAK:
 			DEBUG_PRINTF("BREAK\n");
+			pressed = false;
 			break;
 		case PS2CodeManager::MAKE:
 			DEBUG_PRINTF("MAKE\n");
+			pressed = true;
 			break;
 		}
+
+		composeKeyboardMsg(msg, keycode, pressed);
+
+		DEBUG_PRINTF("Composed ");
+		for (uint8_t i = 0; i < 8; i++)
+			DEBUG_PRINTF("%X ", msg.buf[i]);
+		DEBUG_PRINTF("\n");
+
+		hid_msg_pipe.push(&msg);
 	}
 }
 
